4e64ed3a6dd23940c1dcce7479cb82277e0fa781
Yunus Emre Uzun <yunus@clofro.com>
2021-07-02 11:22:42 +0300

    Next Environment Variables Config Added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	serverRuntimeConfig: {
+		// Will only be available on the server side
+	},
+	publicRuntimeConfig: {
+		// Will be available on both server and client
+		API_URL: process.env.API_URL,
+	},
 }
