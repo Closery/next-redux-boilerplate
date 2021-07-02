69af200bbfcffb285cd1a033d76ce9ac292f68a9
Yunus Emre Uzun <yunus@clofro.com>
2021-07-02 16:47:16 +0300

    Store Added to Root App Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
+import { Provider } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
+import configureStore from '../store/configureStore'
+
+const store = configureStore()
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<>
+		<Provider store={store}>
 			<Toaster position="top-right" />
 			<Component {...pageProps} />
-		</>
+		</Provider>
 	)
 }
 
