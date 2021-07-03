c6e0f3ca3c98624160f02509b3ef5e5e2efa9e7a
Yunus Emre Uzun <yunus@clofro.com>
2021-07-03 18:00:19 +0300

    Delete Function Added to Detail Page

--- pages/detail/[id].jsx
+++ pages/detail/[id].jsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { GET_ExampleDetail, getError, getExampleDetail, getIsDeleted, getLoadingState } from '../../store/examples'
+import { GET_ExampleDetail, DELETE_Example, getError, getExampleDetail, getIsDeleted, getLoadingState } from '../../store/examples'
 import styles from '../../styles/Home.module.css'
 import Layout from '../../components/Layout'
 import Loading from '../../components/Loading'
@@ -19,6 +19,12 @@ export default function Detail({ id }) {
 		else if (error || isDeleted) router.push('/')
 	}, [exampleDetail, error, isDeleted])
 
+	const DeleteExample = () => {
+		if (exampleDetail.id) {
+			dispatch(DELETE_Example(exampleDetail.id))
+		}
+	}
+
 	return (
 		<Layout title={exampleDetail.description}>
 			<div className={styles.container}>
@@ -33,6 +39,12 @@ export default function Detail({ id }) {
 
 							<p className={styles.description}>
 								<code className={styles.code}>{JSON.stringify(exampleDetail, null, 4)}</code>
+							</p>
+
+							<p className={styles.buttons}>
+								<button className={styles.button} onClick={DeleteExample}>
+									Delete Example
+								</button>
 							</p>
 						</main>
 					)}
