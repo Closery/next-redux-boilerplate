54e62cfac7b9234a6a5c5f1fce4a07f5f4c0db2e
Yunus Emre Uzun <yunus@clofro.com>
2021-07-03 17:14:28 +0300

    Detail Page added

---
+++ pages/detail/[id].jsx
@@ -0,0 +1,48 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { GET_ExampleDetail, getError, getExampleDetail, getIsDeleted, getLoadingState } from '../../store/examples'
+import styles from '../../styles/Home.module.css'
+import Layout from '../../components/Layout'
+import Loading from '../../components/Loading'
+
+export default function Detail({ id }) {
+	const dispatch = useDispatch()
+	const router = useRouter()
+	const error = useSelector(getError)
+	const isDeleted = useSelector(getIsDeleted)
+	const loading = useSelector(getLoadingState)
+	const exampleDetail = useSelector(getExampleDetail)
+
+	useEffect(() => {
+		if (!exampleDetail.id && !error && !isDeleted) dispatch(GET_ExampleDetail(id))
+		else if (error || isDeleted) router.push('/')
+	}, [exampleDetail, error, isDeleted])
+
+	return (
+		<Layout title={exampleDetail.description}>
+			<div className={styles.container}>
+				<main className={styles.main}>
+					{loading ? (
+						<Loading loading={loading} />
+					) : (
+						<main className={styles.main}>
+							<h1 className={styles.title}>
+								Detail Page of <a>{exampleDetail.description}</a>
+							</h1>
+
+							<p className={styles.description}>
+								<code className={styles.code}>{JSON.stringify(exampleDetail, null, 4)}</code>
+							</p>
+						</main>
+					)}
+				</main>
+			</div>
+		</Layout>
+	)
+}
+
+Detail.getInitialProps = ({ query }) => {
+	const { id } = query
+	return { id }
+}
